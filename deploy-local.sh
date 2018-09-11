6c527f669fa43b3bf9559281c80b2ce8da1da397
Donald French <dhfrench@vertisoft.com>
2018-09-11 14:29:57 -0700

    Set proper context for delivery-manager dockerfile

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,8 +1,8 @@
 SHA=$(git rev-parse HEAD)
 
-docker build -t dhf0820/delivery-manager:latest -f ./delivery-manager/Dockerfile ./delivery-manager
-#docker build -t dhf0820/ids-extractor:latest -f ./ids-extractor/Dockerfile ./ids-extractor
-#docker build -t dhf0820/ids-image-manager:latest -f ./ids-image-manager/Dockerfile ./ids-image-manager
+docker build -t dhf0820/delivery-manager:latest -f ./delivery-manager/Dockerfile .
+#docker build -t dhf0820/ids-extractor:latest -f ./ids-extractor/Dockerfile .
+#docker build -t dhf0820/ids-image-manager:latest -f ./ids-image-manager/Dockerfile .
 
 docker push dhf0820/delivery-manager:latest
 #docker push dhf0820/ids-extractor:latest
@@ -17,5 +17,6 @@ docker push dhf0820/delivery-manager:$SHA
 #docker push dhf0820/ids-image-manager:$SHA
 
 kubectl apply -f k8s-local/delivery-manager-deployment.yaml
-#kubectl set image deployments/delivery-manager delivery-manager=dhf0820/delivery-manager:$SHA
+#kubectl set deploymentsl/delivery-manager image-manager=dhf0820/image-manager:$SHA
+
 #kubectl set image deployments/demo-extractor demo-extractor=dhf0820/ids-extractor:$SHA
